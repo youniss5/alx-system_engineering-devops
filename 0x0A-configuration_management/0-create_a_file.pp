8b2df0d2fdfbfd34cf80a19217be9bdc87e8c9fa
youniss5 <ahmedhyouniss@gmail.com>
2023-12-22 03:54:54 -0800

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp
 file ( '/tmp/school':
-	node => '0744,
-	owner => 'www-data,
-	group => 'www-data,
+	node => '0744',
+	owner => 'www-data',
+	group => 'www-data',
 	content => 'I love Puppet'
 )
