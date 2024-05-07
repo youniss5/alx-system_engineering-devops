1694318f4505a0fdfd4c176b51085a3cb4924b5a
youniss5 <ahmedhyouniss@gmail.com>
2024-05-07 23:55:57 +0000

    subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a function that queries API & returns the numb of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
