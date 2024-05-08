b77ccd7448b62c27f8f9fab30f108c243f066f14
youniss5 <ahmedhyouniss@gmail.com>
2024-05-08 00:26:32 +0000

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Returns number of subscribers """
+    if subreddit is None or type(subreddit) is not str:
+        return (0)
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+    head = {'User-Agent': 'my-app'}
+    response = requests.get(url, headers=head, allow_redirects=False)
+
+    try:
+        return (response.json().get("data").get("subscribers"))
+    except Exception:
+        return (0)
