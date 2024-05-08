b4dba0fea29799a32c3715ece0783bac4ba3c203
youniss5 <ahmedhyouniss@gmail.com>
2024-05-08 00:31:42 +0000

    recurse

---
+++ 0x16-api_advanced/recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""returns a list containing the titles of all hot articles """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    user_agent = {'User-agent': 'greg'}
+    sub = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                       .format(subreddit, after), headers=user_agent)
+
+    try:
+        sub = sub.json().get('data')
+        after = sub.get('after')
+        sub = sub.get('children')
+        for obj in sub:
+            hot_list.append(obj['data'].get('title'))
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return(hot_list)
+    except:
+        return(None)
